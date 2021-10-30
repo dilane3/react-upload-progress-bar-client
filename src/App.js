c5119bbb99d442d2a73dc6eb0e2fc45ebd4a7703
dilane3 <komboudilane125@gmail.com>
2021-10-30 08:41:35 +0100

    the client app and server app have been linked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
+import React, {useState, useRef, useCallback} from 'react'
 import './App.css';
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: "http://localhost:5000/api/file"
+})
 
 function App() {
+  const [files, setFiles] = useState([])
+  const [filesUploading, setFilesUploading] = useState([])
+
+  const inputFile = useRef()
+
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0]
+    console.log(file)
+
+    if (file) {
+      const formData = new FormData()
+      const filesUploadingClone = [...filesUploading]
+
+      // adding file in the state
+      let id = new Date().getTime()
+
+      const fileToUpload = {
+        id,
+        name: file.name.split('.')[0],
+        size: file.size
+      }
+
+      filesUploadingClone.push(fileToUpload)
+      setFilesUploading(filesUploadingClone)
+
+      // building of form data
+      formData.append("image", file)
+      formData.append("id", id)
+  
+      try {
+        const response = await instance.post("/upload", formData)
+        
+        const fileUploaded = response.data
+        const index = filesUploadingClone.findIndex(file => file.id === Number(fileUploaded.id))
+
+        console.log(index)
+        console.log(fileUploaded.id)
+        console.log(filesUploading)
+
+        if (index > -1) {
+          const filesClone = [...files]
+
+          filesClone.push({...filesUploadingClone[index]})
+          filesUploadingClone.splice(index, 1)
+
+          setFiles(filesClone)
+          setFilesUploading(filesUploadingClone)
+        }
+        console.log(response.data)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className="header-title">Images Uploader</h3>
+
+      <div className="trigger" onClick={() => inputFile.current.click()}>
+        <img src={require('./images/upload.jpeg').default} alt="upload" />
+
+        <span>Browse a file</span>
+      </div>
+      <input 
+        ref={inputFile} 
+        type="file" 
+        hidden 
+        onChange={handleFileChange}
+        accept="image/*"
+      />
+
+      <section className="files">
+        {
+          filesUploading.map(file => (
+            <UploadingFile key={file.id} name={file.name} />
+          ))
+        }
+
+        <div className="uploaded-files">
+          {
+            files.map(file => (
+              <UploadedFile key={file.id} name={file.name} size={file.size} />
+            ))
+          } 
+        </div>
+      </section>
     </div>
   );
+}
+
+const UploadingFile = ({name}) => {
+  return (
+    <article className="file-uploading">
+      <img src={require('./images/icon-file.jpeg').default} alt="file" />
+
+      <div className="file-uploading--info">
+        <div className="info">
+          <span>{name.length > 6 ? name.substr(0, 6) + "...":name} <i className="bi bi-dot"></i> Uploading</span>
+          <span>50%</span>
+        </div>
+
+        <span className="progress-bar">
+          <span></span>
+        </span>
+      </div>
+    </article>
+  )
+} 
+
+const UploadedFile = ({name, size}) => {
+  return (
+    <article className="file-uploading">
+      <img src={require('./images/icon-file.jpeg').default} alt="file" />
+
+      <div className="file-uploading--info">
+        <div className="info">
+          <span>{name.length > 6 ? name.substr(0, 6) + "...":name} <i className="bi bi-dot"></i> Uploaded</span>
+        </div>
+
+        <span className="file-size">{size}B</span>
+      </div>
+
+      <i className="bi bi-check file-uploaded"></i>
+    </article>
+  )
 }
 
 export default App;
